25d4f700f8977404641f8c6e9c4c2dced5baf8db
Eti Gotliv <etigotliv2134@gmail.com>
2025-01-12 23:33:27 +0200

    Dubugging

---
+++ Day13/Exercise 1 - Dubugging odd or Even/main.py
@@ -0,0 +1,7 @@
+number = int(input("Which number do you want to check?"))
+
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
---
+++ Day13/Exercise 2 = Dubugging Leap Year/main.py
@@ -0,0 +1,13 @@
+year = int(input("Which year do you want to check?"))
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
